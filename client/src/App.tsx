fe1079fc01e385c6c66d96d0fb801dfe3ba8a359
Viraj Bhartiya <vlbhartiya@gmail.com>
2025-11-17 09:48:51 +0530

    feat: add Vercel Analytics integration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { NotificationProvider } from "@/contexts/notification.context"
 import { TransactionPopupProvider } from "@/contexts/transaction-popup.context"
 import AppRoutes from "@/router/routes.route"
+import { Analytics } from "@vercel/analytics/react"
 import { BrowserRouter as Router } from "react-router-dom"
 
 import { Toaster } from "@/components/ui/sonner"
@@ -11,6 +12,7 @@ function App() {
     <NotificationProvider>
       <TransactionPopupProvider>
         <Router>
+          <Analytics />
           <CommandMenu />
           <AppRoutes />
           <Toaster />
